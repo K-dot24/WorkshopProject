cf70febea2015cead3a1eae8e1a5d6d2ff16f9ea
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-12 19:36:26 +0300

    Add first structure for testing architecture
    Adding XUnit testing architecture for the project

---
+++ Version 1/Terminal3/XUnitTestTerminal3/LoginTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestTerminal3
+{
+    public class LoginTest : Terminal3TestCase
+    {
+
+    }
+}
---
+++ Version 1/Terminal3/XUnitTestTerminal3/Terminal3TestCase.cs
@@ -0,0 +1,16 @@
+using System;
+using Terminal3.ServiceLayer;
+using Xunit;
+
+namespace XUnitTestTerminal3
+{
+    public abstract class Terminal3TestCase
+    {
+        private readonly IECommerceSystemInterface _sut;
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
