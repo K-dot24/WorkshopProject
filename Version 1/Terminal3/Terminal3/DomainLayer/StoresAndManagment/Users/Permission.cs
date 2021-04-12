6b0fbb8f4cd0fc6ce5467046c9913588859ae33c
Zoe-Ellins <zoee@post.bgu.ac.il>
2021-04-12 13:38:02 +0300

    blank classes

---
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagment/Users/Permission.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal3.DomainLayer.StoresAndManagment.Users
+{
+    class Permission
+    {
+    }
+}
---
+++ Version 1/Terminal3/Terminal3/ServiceLayer/eCommerceSystemInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal3.ServiceLayer
+{
+    interface eCommerceSystemInterface
+    {
+
+    }
+}
