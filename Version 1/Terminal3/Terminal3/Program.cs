02d7b2b299502315d39b66ff16078efc84fdd810
ZoeEllins <zoezellins@gmail.com>
2021-04-12 12:29:43 +0300

    version 1 init

---
+++ Version 1/Terminal3/Terminal3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Terminal3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
