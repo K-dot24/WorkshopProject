a8ed758cac2741836b3c38c44fcfd27a4d4f1daf
Raz Izak <raziz@post.bgu.ac.il>
2021-04-13 18:02:04 +0300

    Discounts classes: Conditional/Discreet inherits from Visible

--- Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/Policies/DiscountPolicies/ConditionalDiscount.cs
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/Policies/DiscountPolicies/ConditionalDiscount.cs
@@ -1,6 +1,15 @@
-﻿namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.DiscountPolicies
+﻿using System;
+
+namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.DiscountPolicies
 {
-    public class ConditionalDiscount : IDiscountPolicy
+    public class ConditionalDiscount : VisibleDiscount
     {
+        public int ConditionalQuantity { get; }
+
+        public ConditionalDiscount(double precentege, DateTime expirationDate, int conditionalQuantity) : base(precentege, expirationDate)
+        {
+            ConditionalQuantity = conditionalQuantity;
+        }
+
     }
 }
