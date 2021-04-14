7c85086cfbb7bcbc9a5014df56797e4646d8b2e5
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-14 13:23:15 +0300

    Add unique username constrain implementation
    Add new project for unit testing due to unfinish acceptance test project which could not be built

--- Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Users/UsersAndPermissionsFacade.cs
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Users/UsersAndPermissionsFacade.cs
@@ -7,6 +7,8 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Users
 {
     public interface IUsersAndPermissionsFacade
     {
+        Result<RegisteredUser> Register(String email, String password);
+        bool isUniqueEmail(string email);
     }
 
     public class UsersAndPermissionsFacade : IUsersAndPermissionsFacade
@@ -18,19 +20,40 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Users
             this.registeredUsers = new LinkedList<RegisteredUser>();
         }
 
-        Result<Object> Register(String email, String password)
+        /// <summary>
+        /// Regiter user to the system using given Email and Password.
+        /// the registration will be success only if the enail is unique and not in use by other user
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>Result object hold the execStatus</returns>
+        public Result<RegisteredUser> Register(String email, String password)
         {
-            if (isUniqueEmail(email)) 
+            if (isUniqueEmail(email))
             {
                 RegisteredUser newUser = new RegisteredUser(email, password);
                 this.registeredUsers.AddLast(newUser);
-                return new Result<Object>($"{email} is registered as new user", true, newUser);
+                return new Result<RegisteredUser>($"{email} is registered as new user", true, newUser);
+            }
+            else {
+                return new Result<RegisteredUser>($"{email} is aleady in user\n Please use different email", false,null);
             }
         }
 
-        private bool isUniqueEmail(string email)
+        /// <summary>
+        /// util function in order to check the the given email is not in use by other user
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>
+        /// boolean wwther the email is unique or not
+        /// </returns>
+        public bool isUniqueEmail(string email)
         {
-            throw new NotImplementedException();
+            foreach (RegisteredUser user in registeredUsers)
+            {
+                if (user.Email.Equals(email)) { return false; }
+            }
+            return true;
         }
     }
 }
---
+++ Version 1/Terminal3/Terminal3Tests/DomainLayer/StoresAndManagement/Users/UsersAndPermissionsFacadeTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using Terminal3.DomainLayer.StoresAndManagement.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal3.DomainLayer.StoresAndManagement.Users.Tests
+{
+    public class UsersAndPermissionsFacadeTests
+    {
+        [Theory()]
+        [Trait("Category", "Unit")]
+        [InlineData("tomer@gmail.com", "raz@gmail.com", true)]
+        [InlineData("tomer@gmail.com", "tomer@gmail.com", false)]
+        public void RegisterTest(String usedEmail, String requestedEmail, Boolean expectedResult)
+        {
+            UsersAndPermissionsFacade facade = new UsersAndPermissionsFacade();
+            Assert.True(facade.Register(usedEmail, "123123").ExecStatus);
+            Assert.Equal(facade.Register(requestedEmail, "123123").ExecStatus, expectedResult);
+        }
+    }
+}
