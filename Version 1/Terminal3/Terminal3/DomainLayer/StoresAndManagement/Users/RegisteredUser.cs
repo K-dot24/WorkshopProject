8823d8b3947c3dc408e0474dda93c9b61a7a13f6
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-15 21:37:04 +0300

    implement Login and Logout mechanism
    Add Login and Logout function in RegisteredUser and all relevant facades and interfaces
    Add LoggedIn boolean in RegisteredUser
    Add unit test for login and logout functions

--- Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Users/RegisteredUser.cs
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Users/RegisteredUser.cs
@@ -4,16 +4,51 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Users
 {
     public class RegisteredUser : User
     {
-        public string UserId { get; }
-        public string Email { get; }
-        public string Password { get; }
+        //Properties
+        public String UserId { get; }
+        public String Email { get; }
+        public String Password { get; }
+        public Boolean LoggedIn { get; set; }
 
-
+        //Constructor
         public RegisteredUser(String email , String password)
         {
             this.UserId = Service.GenerateId();
             this.Email = email;
             this.Password = password;
-        } 
+            this.LoggedIn = false;
+        }
+        
+        //Methods
+        public Result<RegisteredUser> Login(String password) {
+            if (LoggedIn) {
+                //User already logged in
+                return new Result<RegisteredUser>($"{this.Email} already logged in", false, null);
+            }
+            if (Password.Equals(password))
+            {
+                //Correct paswword
+                LoggedIn = true;
+                return new Result<RegisteredUser>($"{this.Email} is Logged in\n", true, this);
+            }
+            else
+            {
+                //Incorrect password
+                return new Result<RegisteredUser>($"Incorrect password provided for {this.Email}\n", false, null);
+
+            }
+        }
+        public Result<RegisteredUser> LogOut() {
+            if (!LoggedIn)
+            {
+                //User already logged out
+                return new Result<RegisteredUser>($"{this.Email} already logged out", false, null);
+            }
+            else 
+            {
+                LoggedIn = false;
+                return new Result<RegisteredUser>($"{this.Email} is Logged out\n", true, this);
+            }           
+        }
     }
 }
