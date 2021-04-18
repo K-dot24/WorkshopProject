eb3641d8e975e676b704dfcca15ade5d4c0f7f01
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-19 01:25:17 +0300

    Add mock for delivery system

--- Version 1/Terminal3/Terminal3/ExternalSystems/PaymentSystem.cs
+++ Version 1/Terminal3/Terminal3/ExternalSystems/PaymentSystem.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace Terminal3.ExternalSystems
 {
-
+    //Mock for payment system
     public static class PaymentSystem
     {
         public static bool Pay(string userID, double amount)
         {
             bool[] bools = { true, false };
-            return bools[new Random().Next(0, 1)];
+            return bools[new Random().Next(0, 2)];
         }
     }
 }
