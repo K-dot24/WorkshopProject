b279ffeb7ca41cc7fe89fcddee95567197dde2da
igorgorb1999 <igorgorb@post.bgu.ac.il>
2021-04-18 22:08:15 +0300

    Updated the logging destination files, and cleaned up some comments

--- Version 1/Terminal3/Terminal3/Program.cs
+++ Version 1/Terminal3/Terminal3/Program.cs
@@ -6,8 +6,6 @@ namespace Terminal3
     {
         static void Main(string[] args)
         {
-            DomainLayer.Logger.LogInfo("My Log Info");
-            DomainLayer.Logger.LogError("My Error Info");
             Console.WriteLine("Hello World!");
             Console.ReadKey();
             
