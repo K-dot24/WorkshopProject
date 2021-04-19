535ef2691c955b879097b742d011cbf5512ab041
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-19 13:25:21 +0300

    Update RegisteredUserController.cs

--- Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/RegisteredUserController.cs
+++ Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/RegisteredUserController.cs
@@ -18,18 +18,18 @@ namespace Terminal3.ServiceLayer.Controllers
             this.StoresAndManagementInterface = storesAndManagementInterface;
         }
         #region Methods
-        public Result<UserDAL> Login(String email, String password) { }
-        public Result<Boolean> LogOut(String email);
-        public Result<Object> SearchStore(IDictionary<String, Object> details);
-        public Result<List<ProductDAL>> SearchProduct(IDictionary<String, Object> productDetails);
-        public Result<Boolean> AddProductToCart(String userID, String ProductID, int ProductQuantity, String StoreID);   // Redundent ?
-        public Result<ShoppingCartDAL> GetUserShoppingCart(String userID);
-        public Result<Dictionary<String, int>> GetUserShoppingBag(String userID, String shoppingBagID); //Dictionary<pid , countity>
-        public Result<Boolean> UpdateShoppingCart(String userID, String shoppingBagID, String productID, int quantity);
-        public Result<Object> Purchase(String userID, IDictionary<String, Object> paymentDetails, IDictionary<String, Object> deliveryDetails);
-        public Result<HistoryDAL> GetUserPurchaseHistory(String userID);
-        public Result<int> GetTotalShoppingCartPrice(String userID);
-        public Result<StoreDAL> OpenNewStore(String storeName, String userID);
+        public Result<UserDAL> Login(String email, String password) { throw new NotImplementedException(); }
+        public Result<Boolean> LogOut(String email) { throw new NotImplementedException(); }
+        public Result<Object> SearchStore(IDictionary<String, Object> details) { throw new NotImplementedException(); }
+        public Result<List<ProductDAL>> SearchProduct(IDictionary<String, Object> productDetails) { throw new NotImplementedException(); }
+        public Result<Boolean> AddProductToCart(String userID, String ProductID, int ProductQuantity, String StoreID) { throw new NotImplementedException(); }   // Redundent ?
+        public Result<ShoppingCartDAL> GetUserShoppingCart(String userID) { throw new NotImplementedException(); }
+        public Result<Dictionary<String, int>> GetUserShoppingBag(String userID, String shoppingBagID) { throw new NotImplementedException(); } //Dictionary<pid , countity>
+        public Result<Boolean> UpdateShoppingCart(String userID, String shoppingBagID, String productID, int quantity) { throw new NotImplementedException(); }
+        public Result<Object> Purchase(String userID, IDictionary<String, Object> paymentDetails, IDictionary<String, Object> deliveryDetails) { throw new NotImplementedException(); }
+        public Result<HistoryDAL> GetUserPurchaseHistory(String userID) { throw new NotImplementedException(); }
+        public Result<int> GetTotalShoppingCartPrice(String userID) { throw new NotImplementedException(); }
+        public Result<StoreDAL> OpenNewStore(String storeName, String userID) { throw new NotImplementedException(); }
 
         #endregion
     }
