440adcbaaf29cfe9c81c5d3a6e1cc2f8e68dc38c
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-19 19:12:56 +0300

    distribute IECInterface (#118)

--- Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/RegisteredUserController.cs
+++ Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/RegisteredUserController.cs
@@ -7,8 +7,15 @@ using Terminal3.DomainLayer.StoresAndManagement;
 
 namespace Terminal3.ServiceLayer.Controllers
 {
+    public interface IRegisteredUserInterface
+    {
+        Result<RegisteredUserDAL> Login(String email, String password);
+        Result<Boolean> LogOut(String email);
+        Result<StoreDAL> OpenNewStore(String storeName, String userID);
+    }
+
     //Extend GuestUser functionality to the st of function that RegisterUser can do
-    public class RegisteredUserController : GuestUserController
+    public class RegisteredUserController : GuestUserController , IRegisteredUserInterface
     {
 
         //Constructor
