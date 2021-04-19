789100c4346eed60d3b100a498cbc3c8752c65b2
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-19 15:35:52 +0300

    Update StoreStaffController.cs

--- Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/StoreStaffController.cs
+++ Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/StoreStaffController.cs
@@ -16,6 +16,18 @@ namespace Terminal3.ServiceLayer.Controllers
             this.StoresAndManagementInterface = storesAndManagementInterface;
         }
         #region Methods
+
+        Result<ProductDAL> AddProductToStore(String userID, String storeID, String productName, double price, int initialQuantity, String category);
+        Result<Boolean> RemoveProductFromStore(String userID, String storeID, String productID);
+        Result<ProductDAL> EditProductDetails(String userID, String storeID, String productID, IDictionary<String, Object> details);
+        Result<Boolean> AddStoreOwner(String addedOwnerID, String currentlyOwnerID, String storeID);
+        Result<Boolean> AddStoreManager(String addedManagerID, String currentlyOwnerID, String storeID);
+        Result<Boolean> SetPermissions(String storeID, String managerID, String ownerID, LinkedList<int> permissions);
+        Result<Boolean> RemovePermissions(String storeID, String managerID, String ownerID, LinkedList<int> permissions);
+        Result<Dictionary<UserDAL, PermissionDAL>> GetStoreStaff(String ownerID, String storeID);
+        Result<HistoryDAL> GetStorePurchaseHistory(String ownerID, String storeID);
+        Result<Boolean> RemoveStoreManager(string removedManagerID, string currentlyOwnerID, string storeID);
+
         #endregion
     }
 }
