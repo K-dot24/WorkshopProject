e75e9ee05a4f51d530a7a7167a8a44c09c7f9d90
shakedshmar <shakedshmar@gmail.com>
2021-04-20 11:14:06 +0300

    new project try

--- Version 1/Terminal3/Terminal3WebAPI/Controllers/Terminal3Controller.cs
+++ Version 1/Terminal3/Terminal3WebAPI/Controllers/Terminal3Controller.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Terminal3.ServiceLayer;
 
 namespace Terminal3WebAPI.Controllers
 {
     public class Terminal3Controller : ApiController
     {
+        public ECommerceSystem system = new ECommerceSystem(); 
+
         // GET: api/Terminal3
         public IEnumerable<string> Get()
         {
