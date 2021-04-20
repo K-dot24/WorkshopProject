cb034aec155ecf5834006116c95c801a4146dafb
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-04-20 14:17:46 +0300

    Add get and add reviews

--- Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/RegisteredUserController.cs
+++ Version 1/Terminal3/Terminal3/ServiceLayer/Controllers/RegisteredUserController.cs
@@ -12,6 +12,7 @@ namespace Terminal3.ServiceLayer.Controllers
         Result<RegisteredUserDAL> Login(String email, String password);
         Result<Boolean> LogOut(String email);
         Result<StoreDAL> OpenNewStore(String storeName, String userID);
+        Result<Boolean> AddProductReview(String userID, String storeID, String productID, String review);
     }
 
     //Extend GuestUser functionality to the st of function that RegisterUser can do
@@ -27,6 +28,10 @@ namespace Terminal3.ServiceLayer.Controllers
         }
         public Result<Boolean> LogOut(String email) { return StoresAndManagementInterface.LogOut(email); }
         public Result<StoreDAL> OpenNewStore(String storeName, String userID) { return StoresAndManagementInterface.OpenNewStore(storeName, userID); }
+        public Result<bool> AddProductReview(string userID, string storeID, string productID, string review)
+        {
+            return StoresAndManagementInterface.AddProductReview(userID, storeID, productID, review);
+        }
         #endregion
     }
 }
