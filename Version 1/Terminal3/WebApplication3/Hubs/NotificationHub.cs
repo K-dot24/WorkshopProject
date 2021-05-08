c56ad006eb1ff072ea27ddbff306c069914f8f92
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-05-08 16:01:42 +0300

    a

---
+++ Version 1/Terminal3/WebApplication3/Hubs/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Terminal3.DomainLayer.StoresAndManagement;
+
+namespace Terminal3WebAPI.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task SendMessage(Notification message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+    }
+}
