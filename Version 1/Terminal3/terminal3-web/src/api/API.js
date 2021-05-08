115ac8be664e30d979523a40957ed69d1a2c2786
shakedshmar <shakedshmar@gmail.com>
2021-05-08 17:58:48 +0300

    Update API.js

--- Version 1/Terminal3/terminal3-web/src/api/API.js
+++ Version 1/Terminal3/terminal3-web/src/api/API.js
@@ -32,6 +32,8 @@ export const products_image_url = 'https://i.ibb.co/HxrQmhn/price-tag.jpg';
 
 // FuncName().then(response => response.json().then(json => setStateName(json))).catch(err => console.log(err));
 
+//#region DataController
+
 export function GetAllStoresToDisplay() {
     return fetch(`https://localhost:5000/api/Data/GetAllStoresToDisplay`, {
         method: 'GET',
@@ -43,14 +45,6 @@ export function GetAllStoresToDisplay() {
     });
 }
 
-// export async function GetAllStoresToDisplay() {
-//     const response = await fetch('https://localhost:5000/api/Data/GetAllStoresToDisplay');
-//     const json = await response.json();
-
-//     console.log(json);
-//     return json;
-// }
-
 export function GetAllProductByStoreIDToDisplay(storeId) {
     return fetch(`https://localhost:5000/api/Data/GetAllProductByStoreIDToDisplay/${storeId}`, {
         method: 'GET',
@@ -61,6 +55,18 @@ export function GetAllProductByStoreIDToDisplay(storeId) {
     })
 }
 
+//#endregion
+
+
+//#region GuestUserController
+
+
+/// Register new user to the system
+/// Template of valid JSON:
+/// {
+///     "Email":"string",
+///     "Password":"string"
+/// }
 export function Register(email, password) {
     return fetch(`https://localhost:5000/api/GuestUser/Register`, {
         method: 'POST',
@@ -72,8 +78,40 @@ export function Register(email, password) {
     })
 }
 
+
+/// search store by attributes
+/// Template of valid JSON
+/// {
+///     "Name":"string",
+///     "rating":double
+/// }
+/// NOTE: fields are optionals
+export function SearchStore() {
+    return fetch(`https://localhost:5000/api/GuestUser/SearchStore`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: { Name, rating },    //TODO
+    })
+}
+
+
+
+//#endregion
+
+
+//#region RegisteredUserController
+
+// Login to the system
+// Template of valid JSON:
+// {
+//     "Email":"string",
+//     "Password":"string"
+// } 
 export function Login(email, password) {
-    return fetch(`https://localhost:5000/api/GuestUser/Login`, {
+    return fetch(`https://localhost:5000/api/RegisteredUser/Login`, {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
@@ -82,6 +120,93 @@ export function Login(email, password) {
         body: { email, password },
     })
 }
+
+export function Logout(email) {
+    return fetch(`https://localhost:5000/api/RegisteredUser/Logout/${email}`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: { password },
+    })
+}
+
+
+/// Open new store in the system
+/// </summary>
+/// Template of valid JSON:
+/// {
+///     "storeName":"string",
+///     "userID":"string"
+/// }
+export function OpenNewStore(email) {
+    return fetch(`https://localhost:5000/api/RegisteredUser/OpenNewStore`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: { storeName, userID },
+    })
+}
+
+
+/// Adding a new review on a product
+/// Template of valid JSON:
+/// {
+///     "userID":"string",
+///     "storeID":"string"
+///     "productID":"string"
+///     "review":"string"
+/// }
+export function AddProductReview(email) {
+    return fetch(`https://localhost:5000/api/RegisteredUser/AddProductReview`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: { userID, storeID, productID, review },
+    })
+}
+
+//#endregion
+
+
+//#region StoreStaffController
+
+
+
+
+
+//#endregion
+
+
+//#region SystemAdminController
+
+
+
+
+
+//#endregion
+
+
+
+
+
+// export async function GetAllStoresToDisplay() {
+//     const response = await fetch('https://localhost:5000/api/Data/GetAllStoresToDisplay');
+//     const json = await response.json();
+
+//     console.log(json);
+//     return json;
+// }
+
+
+
+
+
 
 
 
