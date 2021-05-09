2444c1bcb1688356a055b9b8510810b656be7b44
kmkm133 <kfiroshy@walla.com>
2021-05-09 22:42:45 +0300

    added not implemented functions from the interface to the other purchase policies

--- Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/Policies/PurchasePolicies/Auction.cs
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/Policies/PurchasePolicies/Auction.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using Terminal3.DomainLayer.StoresAndManagement.Users;
 
 namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.PurchasePolicies
 {
@@ -10,6 +12,8 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.PurchasePoli
         public Double StartingPrice { get; }
         public Tuple<Double, String> LastOffer { get; }    // Customer offer <price, UserID>
 
+        public string Id => throw new NotImplementedException();
+
         public Auction(DateTime closingTime, Double startingPrice)
         {
             ClosingTime = closingTime;
@@ -20,6 +24,21 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.PurchasePoli
         public Result<double> CalculatePrice(Product product, int quantity)
         {
             throw new System.NotImplementedException();
+        }
+
+        public Result<bool> IsConditionMet(ConcurrentDictionary<Product, int> bag, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<bool> AddPolicy(IPurchasePolicy policy, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Result<bool> RemovePolicy(string id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
