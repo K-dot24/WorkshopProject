45bfd594697783298b4737b73192bfd391016ec9
igorgorb1999 <igorgorb@post.bgu.ac.il>
2021-05-09 21:32:00 +0300

    initial test

---
+++ Version 1/Terminal3/Terminal3Tests/DomainLayer/StoresAndManagement/Stores/PolicyManagerTests/DiscountPolicyTests/DiscountCalculationTests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Terminal3.DomainLayer.StoresAndManagement.Stores.Policies;
+
+namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Tests
+{
+    public class DiscountCalculationTests
+    {
+
+        public PolicyManager PolicyManager { get; }
+
+        public Dictionary<String, Product> Products { get; }
+
+        public DiscountCalculationTests()
+        {
+            PolicyManager = new PolicyManager();
+            Products = new Dictionary<string, Product>();
+            Products.Add("Bread", new Product("Bread", 10, 100, "Bakery", new LinkedList<string>()));
+            Products.Add("Milk", new Product("Milk", 15, 100, "Dairy", new LinkedList<string>()));
+        }
+
+        [Theory()]
+        [Trait("Category", "Unit")]
+        [InlineData("Bread", 5, 40)]
+        [InlineData("Bread", 1, 10)]
+        [InlineData("Milk", 5, 75)]
+        public void CalculatePriceMinProduct(String productname, int sum, Double expectedPrice)
+        {
+            
+        }
+
+    }
+}
