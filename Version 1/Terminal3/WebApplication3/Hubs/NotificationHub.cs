2e8067bc758138d0efbc952b9b15a6d34ee24e43
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-05-10 17:11:34 +0300

    add identification process

--- Version 1/Terminal3/WebApplication3/Hubs/NotificationHub.cs
+++ Version 1/Terminal3/WebApplication3/Hubs/NotificationHub.cs
@@ -11,22 +11,23 @@ namespace Terminal3WebAPI.Hubs
     public class NotificationHub : Hub<IClient>
     {
         private static ConnectionMapping<string> _connections = new ConnectionMapping<string>(); //userid to connectionid
+        private static ConnectionMapping<string> GuestConnections = new ConnectionMapping<string>(); //userid to connectionid
 
         public override Task OnConnectedAsync()
         {
-            string name = Context.User.Identity.Name;
-            Console.WriteLine($"OnConnectedAsync name:{name}");
+            //string name = Context.User.Identity.Name;
+            //Console.WriteLine($"OnConnectedAsync name:{name}");
 
-            _connections.Add(name, Context.ConnectionId);
+            //_connections.Add(name, Context.ConnectionId);
 
             return base.OnConnectedAsync();
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            string name = Context.User.Identity.Name;
+            //string name = Context.User.Identity.Name;
 
-            _connections.Remove(name);
+            //_connections.Remove(name);
 
             return base.OnDisconnectedAsync(exception);
         }
@@ -34,6 +35,16 @@ namespace Terminal3WebAPI.Hubs
         public async Task SendMessage(ChatMessage message)
         {
             await Clients.All.ReceiveMessage(message);
+        }
+
+        public async Task Identify(Identifier message)
+        {
+            GuestConnections.Add(message.UserID, Context.ConnectionId);
+            Console.WriteLine($"New Connection: UserID:{message.UserID}, ConnectionID:{Context.ConnectionId}");
+            ChatMessage m = new ChatMessage();
+            m.ClientId = message.UserID;
+            m.Message = $"User Connection ID: {Context.ConnectionId}";
+            await Clients.Client(Context.ConnectionId).ReceiveMessage(m);
         }
     }
 }
---
+++ Version 1/Terminal3/WebApplication3/Models/Identifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Terminal3WebAPI.Models
+{
+    public class Identifier
+    {
+        public String UserID { get; set; }
+    }
+}
