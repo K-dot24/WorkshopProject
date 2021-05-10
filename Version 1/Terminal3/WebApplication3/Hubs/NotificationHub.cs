a20353251f35609150785091efc7089233e8e1e6
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-05-10 20:05:09 +0300

    opening new project for SignalR

--- Version 1/Terminal3/WebApplication3/Hubs/NotificationHub.cs
+++ Version 1/Terminal3/WebApplication3/Hubs/NotificationHub.cs
@@ -12,6 +12,7 @@ namespace Terminal3WebAPI.Hubs
     {
         private static ConnectionMapping<string> _connections = new ConnectionMapping<string>(); //userid to connectionid
         private static ConnectionMapping<string> GuestConnections = new ConnectionMapping<string>(); //userid to connectionid
+        private static ConnectionMapping<string> RegisteredConnections = new ConnectionMapping<string>(); //userid to connectionid
 
         public override Task OnConnectedAsync()
         {
@@ -43,6 +44,26 @@ namespace Terminal3WebAPI.Hubs
             Console.WriteLine($"New Connection: UserID:{message.UserID}, ConnectionID:{Context.ConnectionId}");
             ChatMessage m = new ChatMessage();
             m.ClientId = message.UserID;
+            m.Message = $"User Connection ID: {Context.ConnectionId}";
+            await Clients.Client(Context.ConnectionId).ReceiveMessage(m);
+        }
+        public async Task Login(SignalRLoginModel message)
+        {
+            GuestConnections.Remove(message.oldUserID); //Clearing the old etry
+            RegisteredConnections.Add(message.newUserID, Context.ConnectionId);
+            Console.WriteLine($"New Connection: UserID:{message.newUserID}, ConnectionID:{Context.ConnectionId}");
+            ChatMessage m = new ChatMessage();
+            m.ClientId = message.newUserID;
+            m.Message = $"User Connection ID: {Context.ConnectionId}";
+            await Clients.Client(Context.ConnectionId).ReceiveMessage(m);
+        }
+        public async Task Logout(SignalRLoginModel message)
+        {
+            RegisteredConnections.Remove(message.oldUserID); //Clearing the old etry
+            GuestConnections.Add(message.newUserID, Context.ConnectionId);
+            Console.WriteLine($"New Connection: UserID:{message.newUserID}, ConnectionID:{Context.ConnectionId}");
+            ChatMessage m = new ChatMessage();
+            m.ClientId = message.newUserID;
             m.Message = $"User Connection ID: {Context.ConnectionId}";
             await Clients.Client(Context.ConnectionId).ReceiveMessage(m);
         }
