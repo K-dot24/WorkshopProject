04126df07a6bd4d5970a52f436f912b2482d20a8
kmkm133 <kfiroshy@walla.com>
2021-05-12 02:08:59 +0300

    changes to conditional policy for acceptance tests

--- Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/Policies/PurchasePolicies/ConditionalPolicy.cs
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/Policies/PurchasePolicies/ConditionalPolicy.cs
@@ -8,21 +8,29 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.PurchasePoli
 {
     public class ConditionalPolicy : IPurchasePolicy
     {
-        public IPurchasePolicy PreCond { get; }
-        public IPurchasePolicy Cond { get; }
-        public string Id { get; }
+        public IPurchasePolicy PreCond { get; set; }
+        public IPurchasePolicy Cond { get; set; }
+        public string Id { get;}
 
-        public ConditionalPolicy(IPurchasePolicy preCond, IPurchasePolicy cond, string id = "")
+        public ConditionalPolicy(string id = "")
         {
             this.Id = id;
             if (id.Equals(""))
                 this.Id = Service.GenerateId();
+            this.PreCond = null;
+            this.Cond = null;
+        }
+
+        public ConditionalPolicy(IPurchasePolicy preCond, IPurchasePolicy cond, string id = "") : this(id)
+        {            
             this.PreCond = preCond;
             this.Cond = cond;
         }
 
         public Result<bool> IsConditionMet(ConcurrentDictionary<Product, int> bag, User user)
         {
+            if(this.PreCond == null || this.Cond == null)
+                return new Result<bool>("", true, false);
             if (this.PreCond.IsConditionMet(bag, user).Data)
                 if (!this.Cond.IsConditionMet(bag, user).Data)                
                     return new Result<bool>("", true, false);
@@ -32,13 +40,39 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Stores.Policies.PurchasePoli
         public Result<bool> AddPolicy(IPurchasePolicy policy, string id)
         {
             if (this.Id.Equals(id))
-                return new Result<bool>("Cannot add a policy to this type of policy", false, false);
+            {
+                if (this.PreCond == null)
+                    this.PreCond = policy;
+                else if (this.Cond == null)
+                    this.Cond = policy;
+                else
+                    return new Result<bool>("Cannot add a policy to this type of policy", false, false);
+            }
+            else
+            {
+                if (this.PreCond != null)
+                {
+                    Result<bool> curr = this.PreCond.AddPolicy(policy, id);
+                    if (!curr.ExecStatus)
+                        return curr;
+                    if (curr.Data)
+                        return new Result<bool>("", true, true);
+                }
+                if (this.Cond != null)
+                {
+                    Result<bool> curr = this.Cond.AddPolicy(policy, id);
+                    if (!curr.ExecStatus)
+                        return curr;
+                    if (curr.Data)
+                        return new Result<bool>("", true, true);
+                }
+            }
             return new Result<bool>("", true, false);
         }
 
         public Result<bool> RemovePolicy(string id)
         {
-            if(PreCond.Id.Equals(id) || Cond.Id.Equals(id))
+            if((PreCond != null && PreCond.Id.Equals(id)) || (Cond != null && Cond.Id.Equals(id)))
                 return new Result<bool>("", false, false);
 
             Result<bool> res = PreCond.RemovePolicy(id);
