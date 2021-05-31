d7721f57e8b36cb72daea719e63d1919aed5df0c
tomerhacham <48476668+tomerhacham@users.noreply.github.com>
2021-05-31 14:26:08 +0300

    fix bug:  not update store owner DB

--- Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/StoresFacade.cs
+++ Version 1/Terminal3/Terminal3/DomainLayer/StoresAndManagement/Stores/StoresFacade.cs
@@ -159,9 +159,9 @@ namespace Terminal3.DomainLayer.StoresAndManagement.Stores
                     if(store.Owners.TryGetValue(currentlyOwnerID,out StoreOwner owner))
                     {
                         //update owner record
-                        var filterowner = Builders<BsonDocument>.Filter.Eq("_id", owner.User.Id);
+                        var filterowner = Builders<BsonDocument>.Filter.Eq("UserId", owner.User.Id) & Builders<BsonDocument>.Filter.Eq("StoreId", store.Id);
                         var updateowner = Builders<BsonDocument>.Update.Set("StoreOwners", owner.getDTO().StoreOwners);
-                        mapper.UpdateStoreOwner(filter, update);
+                        mapper.UpdateStoreOwner(filterowner, updateowner);
                     }
                 }
 
