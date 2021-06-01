ffa9fb56908159d6a8de5954f9dbcd1b7a064d14
shakedshmar <48641562+shakedshmar@users.noreply.github.com>
2021-06-01 23:08:10 +0300

    Config test (#303)
    * delete fix in db
    
    * Invalid config test

--- Version 1/Terminal3/Terminal3Tests1/E2ETests.cs
+++ Version 1/Terminal3/Terminal3Tests1/E2ETests.cs
@@ -20,24 +20,24 @@ namespace Terminal3_E2ETests
             String config_path = @"..\Terminal3\BadMongoUrlConfig.json";
             ECommerceSystem system = new ECommerceSystem(config_path);
 
-            Assert.True(system.Register("shaked@gmail.com", "123").ExecStatus); 
+            Assert.True(system.Register("shaked@gmail.com", "123").ExecStatus);
         }
 
         [Fact]
         public void BadExternalSystemUrlConfig()
         {
             String config_path = @"..\Terminal3\BadExternalSystemConfig.json";
-            ECommerceSystem system =  new ECommerceSystem(config_path);
+            ECommerceSystem system = new ECommerceSystem(config_path);
 
             system.Register("shaked@gmail.com", "123");
             Result<RegisteredUserService> user = system.Login("shaked@gmail.com", "123");
 
 
             Result<StoreService> store = system.OpenNewStore("testStore", user.Data.Id);
-            Result<ProductService> product = system.AddProductToStore(user.Data.Id, store.Data.Id, "testProduct", 10, 1, "Test");           
+            Result<ProductService> product = system.AddProductToStore(user.Data.Id, store.Data.Id, "testProduct", 10, 1, "Test");
 
             // Add product to user shopping bag
-            system.AddProductToCart(user.Data.Id, product.Data.Id ,1 , store.Data.Id);
+            system.AddProductToCart(user.Data.Id, product.Data.Id, 1, store.Data.Id);
 
             IDictionary<String, Object> paymentDetails = new Dictionary<String, Object>();
             IDictionary<String, Object> deliveryDetails = new Dictionary<String, Object>();
@@ -45,6 +45,14 @@ namespace Terminal3_E2ETests
             Result<ShoppingCartService> res = system.Purchase(user.Data.Id, paymentDetails, deliveryDetails);
             Assert.True(res.ExecStatus);
         }
+
+        [Fact]
+        public void InvalidConfig()
+        {
+            String config_path = @"..\Terminal3\InvalidConfig.json";
+            new ECommerceSystem(config_path);
+        }
+
 
     }
 }
